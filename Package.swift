553218ebe934e1e461b3d638ac76ff3f895a6b7c
Marco Eidinger <marco.eidinger@sap.com>
2021-11-22 08:31:35 -0800

    expose binaryTarget as library product

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ScanbotSDK",
-            targets: ["ScanbotSDK"]),
+            name: "ScanbotBarcodeScannerSDK",
+            targets: ["ScanbotBarcodeScannerSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
